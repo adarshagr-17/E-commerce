bc7459c1a55172b48b8896746d4707f15960e921
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2025-06-10 15:44:47 +0530

    seems like product error solved

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ const NewCollections = () =>{
     const [new_collection,setNew_collection]=useState([]);
 
     useEffect(()=>{
-        fetch('http://localhost:4000/newcollection')
+        fetch('https://e-commerce-pvx3.onrender.com/newcollection')
         .then((response)=>response.json())
         .then((data)=>{setNew_collection(data);console.log(data)});
     },[])
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -14,13 +14,13 @@ const ShopContextProvider = (props) => {
     const [cartItems,setCartItems] = useState(getDefaultCart());
     
     useEffect(()=>{
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://e-commerce-pvx3.onrender.com/allproducts')
         .then((response)=>response.json())
         .then((data)=>setAll_Product(data.products))
         .catch(error => console.error("Error fetching products:", error));
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart',{
+            fetch('https://e-commerce-pvx3.onrender.com/getcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
@@ -37,7 +37,7 @@ const ShopContextProvider = (props) => {
     const addToCart = (itemId)=>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addtocart',{
+            fetch('https://e-commerce-pvx3.onrender.com/addtocart',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
@@ -54,7 +54,7 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId)=>{
         setCartItems((prev=>({...prev,[itemId]:prev[itemId]-1})))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
+            fetch('https://e-commerce-pvx3.onrender.com/removefromcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
